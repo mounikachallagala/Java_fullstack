207d685056ddeffe2b89bfa70155cc92d7499528
mounikachallagala <mounikachallagala@gmail.com>
2022-12-02 15:48:01 +0530

    okie

---
+++ Javascrpit/datatypes/two.js
@@ -0,0 +1,14 @@
+let price = 350;
+let rating = 4.9;
+let name = 'shirt';
+let avail = 'y';
+let discount = true;
+let availabaility;
+{
+    console.log(price)
+    console.log(rating)
+    console.log(name)
+    console.log(avail)
+    console.log(true)
+    console.log(availabaility)
+}
